73c3f723dafdb55eb97fcfef3b1001c64489d021
gcorgne <gabriel.corgne@ac-nantes.fr>
2022-11-29 18:27:18 +0100

    logger

--- src/main/java/com/alliander/keycloak/authenticator/KeycloakHeaderAuthenticator.java
+++ src/main/java/com/alliander/keycloak/authenticator/KeycloakHeaderAuthenticator.java
@@ -2,17 +2,9 @@ package com.alliander.keycloak.authenticator;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map.Entry;
 
-import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import org.jboss.logging.Logger;
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.AuthenticationFlowError;
 import org.keycloak.authentication.Authenticator;
@@ -20,10 +12,10 @@ import org.keycloak.models.AuthenticatorConfigModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
-import org.keycloak.events.Details;
 import org.keycloak.models.utils.KeycloakModelUtils;
 import org.keycloak.services.managers.AuthenticationManager;
 import org.keycloak.sessions.AuthenticationSessionModel;
+import org.keycloak.services.ServicesLogger;
 
 /**
  * Created by joris on 25/11/2016.
@@ -33,7 +25,10 @@ import org.keycloak.sessions.AuthenticationSessionModel;
 
 public class KeycloakHeaderAuthenticator implements Authenticator {
 
-    private static Logger logger = Logger.getLogger(KeycloakHeaderAuthenticator.class);
+    // logger using keyloak.services.ServicesLogger
+    private static final ServicesLogger logger = ServicesLogger.LOGGER;
+
+    String Module = "HeaderAuth : ";
 
     public static final String CREDENTIAL_TYPE = "http-header_validation";
 
@@ -47,7 +42,7 @@ public class KeycloakHeaderAuthenticator implements Authenticator {
         if(headerName == null) {
             accessDenied(context, AuthenticationFlowError.UNKNOWN_USER, "Failed to get header config, empty ?");
         } else {
-            logger.debug("HeaderName = " + headerName);
+            logger.debug(Module+"HeaderName = " + headerName);
 
             //Get User from Header
             String headerusername;
@@ -60,15 +55,15 @@ public class KeycloakHeaderAuthenticator implements Authenticator {
             if(headerusername == null) {
                 accessDenied(context, AuthenticationFlowError.UNKNOWN_USER, "Failed to read header");
             } else {
-                logger.debug("User found from Header = " + headerusername);
+                logger.debug(Module+"User found from Header = " + headerusername);
 
                 //Set HeaderUserModel from HeaderUserName
                 UserModel headerusermodel = KeycloakModelUtils.findUserByNameOrEmail(context.getSession(), context.getRealm(), headerusername);
                 if(headerusermodel == null) {
-                    logger.warn("Failed to get ldap user from header = " + headerusername);
+                    logger.warn(Module+"Failed to get ldap user from header = " + headerusername);
                     accessDenied(context, AuthenticationFlowError.UNKNOWN_USER, "Failed to get ldap user from header (" + headerusername + ")");
                 } else {
-                    logger.debug("Keycloak User found from Header = " + headerusername);
+                    logger.debug(Module+"Keycloak User found from Header = " + headerusername);
 
                     //Check if there is already a user in context : Try to get Current User in KEYCLOAK_IDENTITY Cookie
                     String currentusername = "nocurrentuser";
@@ -76,10 +71,10 @@ public class KeycloakHeaderAuthenticator implements Authenticator {
                     try {
                         currentusername = currentusermodel.getUsername();
                     } catch (NullPointerException npe) {
-                        logger.debug("No Current Keycloak User found");
+                        logger.debug(Module+"No Current Keycloak User found");
                     }
                     
-                    logger.debug("Current Keycloak User = "+currentusername);
+                    logger.debug(Module+"Current Keycloak User = "+currentusername);
                     
                     // Check if current user is the same as header user
                     if (currentusername.equals("nocurrentuser") || currentusername.equals(headerusername)) {
@@ -92,13 +87,13 @@ public class KeycloakHeaderAuthenticator implements Authenticator {
                             for (String headerToNoteName : new ArrayList<String>(Arrays.asList(headersToNotes.split("##")))) {
                                 String headerToNoteValue = context.getHttpRequest().getHttpHeaders().getHeaderString(headerToNoteName);
                                 context.getAuthenticationSession().setUserSessionNote(headerToNoteName,headerToNoteValue);
-                                logger.debug("Header " + headerToNoteName + " = " + headerToNoteValue);
+                                logger.debug(Module+"Header " + headerToNoteName + " = " + headerToNoteValue);
                             }
                         }
                         
                         // Remember Me by default
-                        context.getAuthenticationSession().setAuthNote(Details.REMEMBER_ME, "true");
-	                    context.getEvent().detail(Details.REMEMBER_ME, "true");
+                        // context.getAuthenticationSession().setAuthNote(Details.REMEMBER_ME, "true");
+	                    // context.getEvent().detail(Details.REMEMBER_ME, "true");
                         
                         // Set Success
                         context.success();
@@ -110,9 +105,9 @@ public class KeycloakHeaderAuthenticator implements Authenticator {
                         if (authResult != null) {
                             // Removing current user session
                             AuthenticationManager.backchannelLogout(keycloakSession, authResult.getSession(), true);
-                            logger.debug(msg);
+                            logger.debug(Module+msg);
                         } else {
-                            logger.warn(msg+"Failed to logout User");
+                            logger.warn(Module+msg+"Failed to logout User");
                         }
                         accessDenied(context, AuthenticationFlowError.USER_CONFLICT, msg);
                         // context.resetFlow();
@@ -123,7 +118,7 @@ public class KeycloakHeaderAuthenticator implements Authenticator {
     }
 
     private void accessDenied(final AuthenticationFlowContext context, AuthenticationFlowError autherror, String reason) {
-        logger.warn("Access denied : " + reason);
+        logger.warn(Module+"Access denied : " + reason);
         context.failure(autherror, errorResponse(context, autherror));
     }
 
@@ -140,7 +135,6 @@ public class KeycloakHeaderAuthenticator implements Authenticator {
 
     public boolean requiresUser() {
         // We don't need a User to check if an HTTP header is passed or not
-        // return false;
         return false;
     }
 
@@ -150,11 +144,11 @@ public class KeycloakHeaderAuthenticator implements Authenticator {
     }
 
     public void setRequiredActions(KeycloakSession session, RealmModel realm, UserModel user) {
-        logger.debug("setRequiredActions called ... session=" + session + ", realm=" + realm + ", user=" + user);
+        logger.debug(Module+"setRequiredActions called ... session=" + session + ", realm=" + realm + ", user=" + user);
     }
 
     public void close() {
-        logger.debug("close called ...");
+        logger.debug(Module+"close called ...");
     }
 
 
